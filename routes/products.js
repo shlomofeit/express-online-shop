232c57c77d58faf810e43651143e3e4562a04302
Shlomo Feit <11.5sfeit@gmail.com>
2026-07-09 12:18:22 +0300

    Code of products call with query params

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,36 @@
 import express from "express";
+import { readDataFile, writeToFile } from "../data/data.js";
 
+const PRODUCTS = "./data/products.json";
+const CUSTOMERS = "./data/customers.json";
 const router = express.Router();
 
-router.get("", (req, res) => {
-  res.send();
+router.get("", async (req, res) => {
+  try {
+    const { inStock = "all", maxPrice = "all", search = "all" } = req.query;
+
+    let productsData = await readDataFile(PRODUCTS);
+
+    if (inStock === "true") {
+      productsData = productsData.filter((product) => product.stock > 0);
+    }
+    if (maxPrice !== "all") {
+      const maxPriceNum = Number(maxPrice);
+      productsData = productsData.filter(
+        (product) => product.price <= maxPriceNum,
+      );
+    }
+    if (search !== "all") {
+      const upperSearch = search.toUpperCase();
+      productsData = productsData.filter((product) =>
+        product.name.toUpperCase().includes(upperSearch),
+      );
+    }
+
+    res.json(productsData);
+  } catch (error) {
+    throw error;
+  }
 });
 
 export default router;
