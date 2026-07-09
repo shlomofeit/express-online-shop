9a35282c26b769c784fc35d4bc589d3e83773c4e
Shlomo Feit <11.5sfeit@gmail.com>
2026-07-09 16:11:38 +0300

    Code of customer - /account/balance call with query params - GET

--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,25 @@
 import express from "express";
+import { readDataFile, writeToFile } from "../data/data.js";
 
+const PRODUCTS = "./data/products.json";
+const CUSTOMERS = "./data/customers.json";
 const router = express.Router();
 
-router.get("/balance", (req, res) => {
-  res.send();
+router.get("/balance", async (req, res) => {
+  try {
+    const { customerId } = req.query;
+    const data = await readDataFile(CUSTOMERS);
+    const customerById = data.find(
+      (customer) => customer.customerId === customerId,
+    );
+
+    if (!customerById) {
+      return res.status(400).end("not found");
+    }
+    return res.send(customerById.balance);
+  } catch (error) {
+    throw error;
+  }
 });
 
 export default router;
